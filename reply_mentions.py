ed37b073bac0d5dca2248c1cf3a9a90b43287041
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2022-05-01 21:05:47 +0100

    Add files via upload

--- reply_mentions.py
+++ reply_mentions.py
@@ -15,6 +15,8 @@ ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 
 
+
+
 auth = OAuth1UserHandler(
     consumer_key=API_KEY,
     consumer_secret=API_SECRET_KEY,
@@ -59,7 +61,6 @@ def reply_mentions():
             if (
                 (f"{mention_id}\n" not in replied_ids_list)
                 and ("screenshot" in full_text)
-                and (reply not in full_text)
             ):
                 if "light" in full_text:
                     if (" conversation " in full_text) or (" all " in full_text):
@@ -85,13 +86,14 @@ def reply_mentions():
                         image = create_tweet_screenshot_dark(replied_to_id)
                 image.save("screenshot.jpg")
                 media = api.media_upload(filename="screenshot.jpg")
-                api.update_status(
+                response=api.update_status(
                     status=f"@{author} {reply} \n",
                     in_reply_to_status_id=str(mention_id),
                     media_ids=[media.media_id_string],
                 )
                 replied_ids = open("assets/replied_ids.txt", "a")
                 replied_ids.write(f"{mention_id}\n")
+                replied_ids.write(f"{response.id}\n")
                 replied_ids.close()
                 print(mention_id)
             else:
