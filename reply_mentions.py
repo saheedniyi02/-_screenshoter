0d1ab1004ae6bb00fc8f4e6550129c038af421b8
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2022-09-10 08:18:38 +0100

    Update reply_mentions.py

--- reply_mentions.py
+++ reply_mentions.py
@@ -52,11 +52,11 @@ def get_mention_id(mention):
 def reply_mentions():
     bot_name = "_screenshoter"
     count = 2
-    #reply_1 = "Your screenshot can be found below😁!\n\nRemember your commands:\n 'All' command gets all the tweets above the tweet you commented on in the thread.\n\n 'light' and 'dark' commands returns your screenshot in light mode⬜ and dark mode⬛"
-    #reply_2="Your screenshot can be found below 😁!\n\n🤩Want to get your screenshot without using the bot? Visit : screenshoter.live ."
-    #reply=random.choice([reply_1,reply_2])
-    link="https://t.me/cryptoviptrad"
-    reply = "Your screenshot can be found below🤩😁!\n\nAd:\nExclusive Crypto Futures Trading Community. Join the telegram group Now: "+link 
+    reply_1 = "Your screenshot can be found below😁!\n\nRemember your commands:\n 'All' command gets all the tweets above the tweet you commented on in the thread.\n\n 'light' and 'dark' commands returns your screenshot in light mode⬜ and dark mode⬛"
+    reply_2="Your screenshot can be found below 😁!\n\n🤩Want to get your screenshot without using the bot? Visit : screenshoter.live ."
+    reply=random.choice([reply_1,reply_2])
+    #link="https://t.me/cryptoviptrad"
+    #reply = "Your screenshot can be found below🤩😁!\n\nAd:\nExclusive Crypto Futures Trading Community. Join the telegram group Now: "+link 
     mentions = api.mentions_timeline(
         count=count, include_entities=True, tweet_mode="extended"
     )
