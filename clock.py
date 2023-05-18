35533f98779a5fcad5f27e4ec016236817723bdc
Saheed Ayanniyi <64325621+saheedniyi02@users.noreply.github.com>
2023-05-18 18:03:32 +0100

    Add files via upload

--- clock.py
+++ clock.py
@@ -1,1 +1,7 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from reply_mentions import reply_mentions,reply_form, clean_replied
 
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=reply_mentions, trigger="interval", seconds=27)
+scheduler.add_job(func=clean_replied,trigger="interval",seconds=86400)
+scheduler.start()
